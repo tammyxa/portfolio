2d3eec5f79a28809624271c968f32b157a9929b3
Tammy Xaypraseuth <icecoldxD123@gmail.com>
2024-07-09 14:11:01 -0700

    feat: create data folder to hold achievements.json

--- src/components/Achievement/Achievement.tsx
+++ src/components/Achievement/Achievement.tsx
@@ -3,15 +3,33 @@ import styled from "styled-components";
 interface AchievementProps {
   img: string;
   alt: string;
+  title: string;
 }
 
 const AchievementContainer = styled.div``;
 
-export const Achievement = ({ img, alt }: AchievementProps) => {
+const ImageContainer = styled.div`
+  position: relative;
+`;
+
+const DescriptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;
+
+const Title = styled.span``;
+
+export const Achievement = ({ img, alt, title }: AchievementProps) => {
   return (
     <AchievementContainer>
-      <img src={img} alt={alt} />
-      <img src="picture_frame1.png" alt="Picture Frame" />
+      <ImageContainer>
+        <img src={img} alt={alt} />
+        <img src="picture_frame1.png" alt="Picture Frame" />
+      </ImageContainer>
+      <DescriptionContainer>
+        <Title>{title}</Title>
+      </DescriptionContainer>
     </AchievementContainer>
   );
 };
